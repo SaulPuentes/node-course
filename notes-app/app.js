58378d2174554a10e8655ed262376bd7079850c5
SaulPuentes <saul.puentess@gmail.com>
2021-02-21 00:04:53 -0600

    Get input from the user

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,10 @@
 const chalk = require('chalk')
 const getNotes = require('./notes')
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
 
-//
-// Challenge: Use the chalk library in your project
-//
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk into app.js
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-//
-// Bonus: Use docs to mess around with other styles. Make text bold and inversed.
-
-const greenMsg = chalk.green.inverse.bold('Success')
-
-console.log(greenMsg)
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
