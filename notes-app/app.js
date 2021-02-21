9af71cb0e962462cd6ac9f7a663e7f9b7ff204b1
SaulPuentes <saul.puentess@gmail.com>
2021-02-21 00:41:24 -0600

    Argument parsing with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,65 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+//
+// Challenge: Add an option to yargs
+//
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it required and for it to be a tring
+// 3. Log the body value in the handler function
+// 4. Test your work!
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ', argv.title)
+        console.log('Body: ', argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function() {
+        console.log('Listing out all note')
+    }
+})
+
+// Create red command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
