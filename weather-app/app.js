357559cb43280c478d5a0104aa9ad21dba4d0203
Saul Puentes <saul@Sauls-MacBook-Pro.local>
2021-03-03 05:11:20 -0600

    Callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,23 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Philadelphia', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-})
+const address = process.argv[2]
 
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-forecast(40.714, -74.006, (error,  data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+if (!address) {
+  console.log('Please provide an address')
+} else {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error)
+    }
+    
+    forecast(data.latitude, data.longitude, (error,  forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
+  
+      console.log(data.location)
+      console.log(forecastData)
+    })
   })
+}
